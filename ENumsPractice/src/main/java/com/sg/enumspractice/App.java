8c503bce1cb6ebe4432fc7e7063af3ca77ec43e1
Teresa de Castro <decastro.ter@gmail.com>
2022-07-04 14:36:49 -0400

    Prior to making return statements

--- ENumsPractice/src/main/java/com/sg/enumspractice/App.java
+++ ENumsPractice/src/main/java/com/sg/enumspractice/App.java
@@ -5,12 +5,31 @@
  */
 package com.sg.enumspractice;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Teresa
  */
 public class App {
     public static void main(String[] args) {
-        MathOperator[] math  = MathOperator.values();
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("Please to enter a number.");
+        int operand1 = Integer.parseInt(userInput.nextLine());
+        
+        System.out.println("Please enter another number");
+        int operand2 = Integer.parseInt(userInput.nextLine());
+        
+        System.out.println("Please choose whether you want these numbers to plus, minus, multiply or divide.");
+        String mathStr = userInput.nextLine();
+        MathOperator math = MathOperator.valueOf(mathStr.toUpperCase());
+        
+        int sum = 0;
+        switch(math){
+            case PLUS:
+                sum = operand1 + operand2;
+                System.out.println(operand1 + " + " + operand2 + " = " + sum);
+        }
     }
 }
--- ENumsPractice/src/main/java/com/sg/enumspractice/MathOperator.java
+++ ENumsPractice/src/main/java/com/sg/enumspractice/MathOperator.java
@@ -10,5 +10,22 @@ package com.sg.enumspractice;
  * @author Teresa
  */
 public enum MathOperator {
-    PLUS, MINUS, MULTIPLY, DIVIDE
+    PLUS, MINUS, MULTIPLY, DIVIDE;
+    
+     public int calculate(MathOperator operator, int operand1, int operand2) {
+        
+        switch(operator){
+            case PLUS:
+                return operand1 + operand2;
+            case MINUS:
+                return operand1 - operand2;
+            case MULTIPLY:
+                return operand1 * operand2;
+            case DIVIDE:
+                return operand1 / operand2;
+            default:
+                throw new UnsupportedOperationException();
+        }
+    } 
 }
+
