e3cfd9411d374d5355eeb57cc5d66551e1a4e66b
Teresa de Castro <decastro.ter@gmail.com>
2022-07-04 13:02:51 -0400

    Taking a break from this and trying Math

--- ENumsPractice/src/main/java/com/sg/enumspractice2/App.java
+++ ENumsPractice/src/main/java/com/sg/enumspractice2/App.java
@@ -15,44 +15,40 @@ public class App {
     public static void main(String[] args) {
         // asks the user to enter a day of the week
         Scanner userInput = new Scanner(System.in);
-        boolean isWrongAnswer;
         
-        do{
-            isWrongAnswer = false;
-            System.out.println("Please enter a day of the week.");
-            String day = userInput.nextLine();
-
-            // then use a switch statement and your enum to print out 
-            DaysOfTheWeek[] dayOfWeek = DaysOfTheWeek.values();
-            for(DaysOfTheWeek today : dayOfWeek){
-                // how many days there are until Friday
-                // Using Enum in Switch case statement
-                switch(today){
-                    case SUNDAY:
-                        System.out.println("Sunday is FUNday!");
-                        break;
-                    case MONDAY:
-                        System.out.println("Mondays suck.");
-                        break;
-                    case TUESDAY:
-                        System.out.println("Tooney Tuesday!");
-                        break;
-                    case WEDNESDAY:
-                        System.out.println("Wednesdays we wear pink.");
-                        break;
-                    case THURSDAY:
-                        System.out.println("Thurs-ty-day");
-                        break;
-                    case FRIDAY:
-                        System.out.println("Fri-YAYYA!");
-                        break;
-                    case SATURDAY:
-                        System.out.println("Satur-heeeey");
-                        break;
-                }
-            } 
+        System.out.println("Please enter a day of the week.");
+        String day = userInput.nextLine();
+        
+        // then use a switch statement and your enum to print out 
+        DaysOfTheWeek[] dayOfWeek = DaysOfTheWeek.values();
+        for(DaysOfTheWeek today : dayOfWeek){
+            // how many days there are until Friday
+            // Using Enum in Switch case statement
+            switch(today){
+                case SUNDAY:
+                    System.out.println("Sunday is FUNday!");
+                    break;
+                case MONDAY:
+                    System.out.println("Mondays suck.");
+                    break;
+                case TUESDAY:
+                    System.out.println("Tooney Tuesday!");
+                    break;
+                case WEDNESDAY:
+                    System.out.println("Wednesdays we wear pink.");
+                    break;
+                case THURSDAY:
+                    System.out.println("Thurs-ty-day");
+                    break;
+                case FRIDAY:
+                    System.out.println("Fri-YAYYA!");
+                    break;
+                case SATURDAY:
+                    System.out.println("Satur-heeeey");
+                    break;
+            }
             
-        } while(isWrongAnswer);
+        }
     }
      
 }
