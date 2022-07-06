e62557de2a2b32c8e57eb1bfcddd23de8b51030d
Teresa de Castro <decastro.ter@gmail.com>
2022-07-06 12:43:25 -0400

    MonsterDaoTest

--- MonstrousDaoTestingLab/src/test/java/com/sg/testing/dao/implementations/buggy/BadMonsterDaoATest.java
+++ MonstrousDaoTestingLab/src/test/java/com/sg/testing/dao/implementations/buggy/BadMonsterDaoATest.java
@@ -5,6 +5,9 @@
  */
 package com.sg.testing.dao.implementations.buggy;
 
+import com.sg.testing.dao.MonsterDao;
+import com.sg.testing.dao.implementations.AGoodMonsterDao;
+import com.sg.testing.model.Monster;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,30 +19,21 @@ import static org.junit.Assert.*;
  *
  * @author Teresa
  */
-public class BadMonsterDaoATest {
+public class BadMonsterDaoATest extends AGoodMonsterDao{
     
-    public BadMonsterDaoATest() {
+    BadMonsterDaoA testDao;
+    
+    public BadMonsterDaoATest(BadMonsterDaoA dao) {
+        this.testDao = dao;
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     @Test
-    public void testSomeMethod() {
-        fail("The test case is a prototype.");
+    public void addMonster() {
+//        String monsterID = "0001";
+        Monster m monster = new id("0001");
+        monster1.setName("Yuki");
+        
     }
     
 }
