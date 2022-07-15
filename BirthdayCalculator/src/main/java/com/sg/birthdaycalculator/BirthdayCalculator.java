3e4220faeb86267fd2d36357fc35ab1e6f9cd68f
Teresa de Castro <decastro.ter@gmail.com>
2022-07-15 16:04:55 -0400

    Trying to get day of week in Birthday

--- BirthdayCalculator/src/main/java/com/sg/birthdaycalculator/BirthdayCalculator.java
+++ BirthdayCalculator/src/main/java/com/sg/birthdaycalculator/BirthdayCalculator.java
@@ -5,10 +5,34 @@
  */
 package com.sg.birthdaycalculator;
 
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Calendar;
+import java.util.Scanner;
+
 /**
  *
  * @author Teresa
  */
 public class BirthdayCalculator {
-    
+    public static void main(String[] args) {
+        
+        String birthday = "";
+        
+        System.out.println("Welcome to the Magical BirthDAY Calculator!");
+        
+        // Ask someone for their birthday ex: 01/01/2002
+        System.out.println("What's your birthday? (mm-dd-yyyy format)");
+        Scanner userInput = new Scanner(System.in);
+        birthday = userInput.nextLine();
+        // converting format of date
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime().toString());
+        
+        
+        // Tell them the day of the week their birthday falls on.
+        
+        
+        
+    }
 }
